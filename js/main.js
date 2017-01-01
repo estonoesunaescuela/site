defda6f55f4ddd847d1b67c31aea506e9b1f862f
ground.contact <ground.contact@gmail.com>
2017-01-01 18:44:14 +0000

    update date in ajax call

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $.ajax({
         url: "https://script.google.com/macros/s/AKfycbxlj0pvtXo0cjOuGkpZvhoHRXdkTqvCiJzgnNpPIfV-LiNB_jk/exec",
         method: "post",
-        dataType: "json"
+        dataType: "json",
+        data: { email: "ground.contact@gmail.com", user_id: 123456 }
     })
     .done(function(msg) {
         console.log("Data retrieved: " + msg);
