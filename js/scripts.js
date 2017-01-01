1470cf5910d2011159e605fb315a88f7bc65833c
ground.contact <ground.contact@gmail.com>
2017-01-01 20:19:13 +0000

    Add document ready and form data

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
-$.ajax({
-        url: "https://script.google.com/macros/s/AKfycbxlj0pvtXo0cjOuGkpZvhoHRXdkTqvCiJzgnNpPIfV-LiNB_jk/exec",
-        method: "post",
-        dataType: "json",
-        data: { email: "ground.contact@gmail.com", user_id: 9627469}
-    })
-    .done(function(msg) {
-        console.log("Data retrieved: " + msg);
-    })
-    .fail(function(jqXHR) {
-        console.log("Fails: " + jqXHR);
+$(document).ready(function(){
+
+    $('#inputForm').on("submit", function(event) {
+        event.preventDefault();
+        var data = $(this).serialize();
+        console.log(data);
+        $.ajax({
+                url: "https://script.google.com/macros/s/AKfycbxlj0pvtXo0cjOuGkpZvhoHRXdkTqvCiJzgnNpPIfV-LiNB_jk/exec",
+                method: "post",
+                dataType: "json",
+                data: data
+            })
+            .done(function(msg) {
+                //Do sth and present to the user valid information, clean form;
+                console.log("Data retrieved: " + msg);
+            })
+            .fail(function(jqXHR) {
+                console.log("Fails: " + jqXHR);
+            });
+
     });
+};
