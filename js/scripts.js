e80d06d6d0dbe952f43b41c92e78b6611e4ad862
ground.contact <ground.contact@gmail.com>
2017-01-01 20:47:09 +0000

    Add alert message

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+const RETURN_CODES = [100, 200, 300, 400, 500];
+const ALERT_TYPES = ["alert-warning", "alert-success", "alert-warning", "alert-danger", "alert-danger"];
 $(document).ready(function(){
 
     $('#inputForm').on("submit", function(event) {
@@ -14,6 +16,12 @@ $(document).ready(function(){
             .done(function(msg) {
                 //Do sth and present to the user valid information, clean form;
                 console.log("Data retrieved: " + msg);
+                var code = msg.return_code;
+                var index = RETURN_CODES.indexOf(code);
+                $("#alert-message").addClass(ALERT_TYPES[index]);
+                var message = msg.return_message;
+                $("#alert-message").text(message);
+
             })
             .fail(function(jqXHR) {
                 console.log("Fails: " + jqXHR);
