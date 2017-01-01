306b3b39b0f2575b2e5231afaec849d8fb89fd45
ground.contact <ground.contact@gmail.com>
2017-01-01 21:32:11 +0000

    Add show panel

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,7 @@ $(function(){
 $(document).ready(function(){
     $('#inputForm').on("submit", function(event) {
         event.preventDefault();
-        console.log($(this));
         let data = $(this).serialize();
-        console.log(data);
         $.ajax({
                 url: "https://script.google.com/macros/s/AKfycbxlj0pvtXo0cjOuGkpZvhoHRXdkTqvCiJzgnNpPIfV-LiNB_jk/exec",
                 method: "post",
@@ -21,12 +19,12 @@ $(document).ready(function(){
             })
             .done(function(msg) {
                 //Do sth and present to the user valid information, clean form;
-                console.log("Data retrieved: " + msg);
                 var code = msg.return_code;
                 var index = RETURN_CODES.indexOf(code);
                 $("#alert-message").removeClass("alert-warning alert-success alert-danger").addClass(ALERT_TYPES[index]);
                 var message = msg.return_message;
                 $("#alert-text").text(message);
+                $("#alert-message").show();
             })
             .fail(function(jqXHR) {
                 console.log("Fails: " + jqXHR);
